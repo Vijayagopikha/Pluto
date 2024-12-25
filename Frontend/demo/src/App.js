be7b6ea23408cfb32baf9601438a7f68c488a9b0
Vijayagopikha <vijayagopikha@gmail.com>
2024-12-25 18:00:51 +0530

    css

--- Frontend/demo/src/App.js
+++ Frontend/demo/src/App.js
@@ -2,3 +2,17 @@ import Home from './Components/Home';
 import About from './Components/About';
 import Contribute from './Components/Contribute';
 import Materials from './Components/Materials';
+import Navbar from './Components/Navbar';
+
+const App = () => {
+    return(
+       <div>
+           <Navbar />
+           <Home />
+           <About />
+           <Materials />
+           <Contribute />
+       </div>
+    )
+}
+export default App;
--- Frontend/demo/src/Components/Materials.js
+++ Frontend/demo/src/Components/Materials.js
@@ -22,10 +22,10 @@ const Materails = () =>
     return(
         <div className='container'>
             <h1 id = 'materials'>Materials</h1>
-                <div className='search-bar'>
+                <div className='search-btn'>
                 <label htmlFor='search'>Search:</label>
                 <input type='text' placeholder='Search by Subject Name' value={searchQuery} onChange={(e) => setSearchQuery(e.target.value)}
-                />
+                className='search-input'/>
                 </div>
                 <div className='materials-btn'>
                     {filteredMaterials.map((material,index)=>{
